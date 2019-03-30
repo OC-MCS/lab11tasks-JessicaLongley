68361557fdc7a6abf5da00d96eba431bbb3715e0
Jessica Longley <jessica.longley@eagles.oc.edu>
2019-03-30 12:48:48 -0500

    Task 3 and half of task 4 complete

--- NumberList/main.cpp
+++ NumberList/main.cpp
@@ -1,16 +1,53 @@
 #include <iostream>
 #include "NumberList.h"
+#include <fstream>
 using namespace std;
 
 int main()
 {
     NumberList list;
-    list.insertNode(10);
-    list.appendNode(40);
-    list.insertNode(30);
-    list.insertNode(25);
-    list.deleteNode(30);
-    list.displayList();
+    //task 1
+    //list.insertNode(10);
+    //list.appendNode(40);
+    //list.insertNode(30);
+    //list.insertNode(25);
+    //list.deleteNode(30);
+    //list.displayList();
+
+    //task 2
+    //fstream file;
+    //file.open("Numbers.txt");
+    //int temp;
+    //if (file)
+    //{
+    //    while (file >> temp)
+    //    {
+    //        list.insertNode(temp);
+    //    }
+    //    file.close();
+    //}
+    //else
+    //{
+    //    cout << "Error in opening file." << endl;
+    //}
+    //list.displayList();
+
+    NumberList listA, listB;
+    listA.appendNode(10);
+    listA.appendNode(20);
+    listB.appendNode(15);
+    listB.appendNode(40);
+    cout << "list A before assign: " << endl;
+    listA.displayList();
+    cout << "list B before assign: " << endl;
+    listB.displayList();
+    listA = listB;	// assign B to A
+    listB.appendNode(50);
+    cout << "list A after assign: " << endl;
+    listA.displayList();
+    cout << "list B after assign: " << endl;
+    listB.displayList();
+
 
 	return 0;
 }
